feff6b411b685731871cd448b543330b97208aa6
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-11-17 21:23:30 +0500

    Add files via upload

---
+++ Advance JS/class 01/app.js
@@ -0,0 +1,64 @@
+// advance JS
+
+// diff b/w =====> var let & const
+
+// let firstName = "faraz";
+
+// {
+//   let firstName = "Muhammad";
+//   console.log(firstName);
+// }
+
+// console.log(firstName);
+
+// function num() {
+//   const nums = 10;
+//   console.log(nums);
+// }
+
+// num();
+
+// const num = 50;
+
+// num = 100;
+
+// console.log(num);
+
+const obj = {
+  name: "faraz",
+  email: "faraz@gmail.com",
+  city: "karachi",
+};
+
+obj = "Muhammad faraz";
+
+console.log(obj);
+
+const firstName = "Muhammad";
+
+const firstName = "faraz";
+
+console.log(firstName);
+
+console.log(firstName);
+
+const firstName = "faraz";
+
+// var nm = 20;
+
+// greet();
+
+func();   // output: func is not a function
+
+console.log(func);  //output: undefined
+
+var func = function () {
+  console.log("welcome user!!");
+};
+
+
+// function decalration
+
+function abc(){
+
+}
