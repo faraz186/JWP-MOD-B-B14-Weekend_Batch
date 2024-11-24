2e8cc875e2ad345840ebcd6d7aeb2c0392f6ea07
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-11-25 00:12:05 +0500

    Update app.js

--- Advance JS/class 04/app.js
+++ Advance JS/class 04/app.js
@@ -9,7 +9,7 @@ let mobiles = {
       camera: {
         rear: {
           main: 200,
-          ultrawide: 12,  
+          ultrawide: 12,    
           telephoto: 10,
           telephotoPeriscope: 10,
         },
