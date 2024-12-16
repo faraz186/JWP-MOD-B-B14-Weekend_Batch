a80179cc0d057b12eb6d4b0836ca11e2e430391a
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-12-16 18:31:53 +0500

    Add files via upload

---
+++ React JS/class 02/src/App.jsx
@@ -0,0 +1,29 @@
+import { Button } from "./components/Button";
+import Input from "./components/Input";
+import "./App.css";
+
+const App = () => {
+  let username = "faraz";
+
+  return (
+    <div>
+      <Button name="Submit" />
+      <Input />
+      <Button name="Next" />
+      <Button name="Done" />
+      <Button />
+    </div>
+    // <div>
+    //   <Input />
+    //   <Button />
+    //   <h1>Welcome {2 + 4 + 10}</h1>
+    //   <p>
+    //     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero
+    //     perspiciatis consequatur consectetur nihil sapiente excepturi, molestiae
+    //     dolores facilis sed iste.
+    //   </p>
+    // </div>
+  );
+};
+
+export default App;
