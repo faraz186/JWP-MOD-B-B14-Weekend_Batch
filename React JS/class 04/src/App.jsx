f9b4c9da6181bbe0795e7c3d970c5ce41061b076
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-12-23 12:11:00 +0500

    Update App.jsx

--- React JS/class 04/src/App.jsx
+++ React JS/class 04/src/App.jsx
@@ -1,7 +1,7 @@
 import Button from "./Components/Button/Button";
 import Student from "./Components/Student/Student";
 import "./App.css";  
-import { Data } from "./data";
+import { Data } from "./data";  
 import Card from "./Components/Card/Card";  
 
 const App = () => {
