26d03bdbce1a672b33e833296f9fabc5716ecbc1
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-12-29 01:47:08 +0500

    Update App.jsx

--- React JS/class 05/src/App.jsx
+++ React JS/class 05/src/App.jsx
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
-import Header from "./Components/Navbar/Navbar";
+import Header from "./Components/Navbar/Navbar";  
 import Data from "./data";
 import { CiHome } from "react-icons/ci";
 import { IoLogoGoogle } from "react-icons/io5";
