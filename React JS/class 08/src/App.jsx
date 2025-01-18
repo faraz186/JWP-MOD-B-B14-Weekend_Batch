946ab7c9417a62b623ee9db810df4429b8df5a3f
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-01-18 20:50:31 +0500

    registration form & react-router-dom start...

---
+++ React JS/class 08/src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Registration_form from "./Screens/Registration_form";
+import Home from "./Screens/Home";
+import About from "./Screens/About";
+import User from "./Screens/User";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/user" element={<User />} />
+        <Route path="/register_form" element={<Registration_form />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
