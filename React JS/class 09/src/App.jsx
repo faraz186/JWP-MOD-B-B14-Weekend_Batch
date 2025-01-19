0f4d4dbd500ddf72b52ddc1780586bdbdbf16763
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-01-19 21:11:54 +0500

    useParams, searchParams, Navigate, location and dynamic routing complete...

---
+++ React JS/class 09/src/App.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Screens/Home";
+import About from "./Screens/About";
+import User from "./Screens/User";
+import Registration_form from "./Screens/Registration_form";
+import NotFound from "./Screens/NotFound";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        {/* Basic Routing */}
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* <Route path="/user" element={<User />} /> */}
+        <Route path="/form" element={<Registration_form />} />
+        <Route path="*" element={<NotFound />} />
+
+        {/* Dynamic Routing */}
+
+        {/* dynamic params */}
+
+        {/* <Route path="/user/:id" element={<User />} /> */}
+
+        {/* query params */}
+
+        {/* <Route path="/user" element={<User />} /> */}
+
+        {/* Nested Routing */}
+
+        <Route path="/dashboard" element={<Dashboarad />}>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/anncouncement" element={<Anncouncement />} />
+          <Route path="/COntact" element={<COntact />} />
+        </Route>
+      </Routes>
+      
+    </>
+  );
+};
+
+export default App;
