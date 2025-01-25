5dc9a108497ac192097559b8a2c11c05954d1b13
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-01-26 01:20:41 +0500

    Update App.jsx

--- React JS/class 02/src/App.jsx
+++ React JS/class 02/src/App.jsx
@@ -13,16 +13,17 @@ const App = () => {
       <Button name="Done" />
       <Button />
     </div>
-    // <div>
-    //   <Input />
-    //   <Button />
-    //   <h1>Welcome {2 + 4 + 10}</h1>
-    //   <p>
-    //     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero
-    //     perspiciatis consequatur consectetur nihil sapiente excepturi, molestiae
-    //     dolores facilis sed iste.
-    //   </p>
-    // </div>
+    
+    <div>
+      <Input />
+      <Button />
+      <h1>Welcome {2 + 4 + 10}</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero
+        perspiciatis consequatur consectetur nihil sapiente excepturi, molestiae
+        dolores facilis sed iste.
+      </p>
+    </div>
   );
 };
 
