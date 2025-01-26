e711a98be1cdffc9e8435905bd96fc39374da3d2
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-01-27 00:49:22 +0500

    Update App.jsx

--- React JS/class 11/src/App.jsx
+++ React JS/class 11/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Screens/Home";  
-import Dashboard from "./Screens/Dashboard";
+import Dashboard from "./Screens/Dashboard";  
 import Profile from "./Screens/Profile";
 import Contact from "./Screens/Contact";
 import Settings from "./Screens/Settings";
