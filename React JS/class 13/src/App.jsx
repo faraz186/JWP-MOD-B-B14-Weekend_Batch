88ec2ab7337d67c5579fb53b17e8b7369065ada8
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-02-04 00:58:55 +0500

    Update App.jsx

--- React JS/class 13/src/App.jsx
+++ React JS/class 13/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react"; 
+import React from "react";  
 import { Route, Routes } from "react-router-dom";       
 import Home from "./Screens/Home";  
 import Create from "./Screens/Create";
