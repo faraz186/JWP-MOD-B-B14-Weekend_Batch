1af701ec3a309b770694d26a32c936ee482eeb00
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-02-09 23:25:26 +0500

    Login SignUp Auth & DB end to end connectivity...

---
+++ React JS/class 15/src/App.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import SignUp from "./Screens/SignUp";
+import Login from "./Screens/Login";
+import Dashboard from "./Screens/Dashboard";
+import { ToastContainer } from "react-toastify";
+import ProtectedRoute from "./components/ProtectedRoute";
+import AuthRoute from "./components/AuthRoute";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+
+        <Route element={<AuthRoute />}>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
+      </Routes>
+
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+    </>
+  );
+};
+
+export default App;
