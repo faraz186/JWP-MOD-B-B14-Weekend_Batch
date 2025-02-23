5df541efe9bd487503ed57312ef72a618214da06
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-02-23 22:54:34 +0500

    server create and local api's

---
+++ Backend/class 01/app.js
@@ -0,0 +1,29 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 6000;
+
+
+
+app.get("/user", (request, response) => {
+  response.send({
+    name: "faraz",
+    age: 24,
+    city: "karachi",
+  });
+});
+
+
+
+app.get("/", (request, response) => {
+  response.send("Server Running on /");
+});
+
+
+
+// Create server with expressjs
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
